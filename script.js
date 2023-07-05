c90f1f9b36b2e473dc16df65018482a1089dce79
tomazlanza <lanza.tomaz@gmail.com>
2023-07-05 16:43:09 -0300

    Before reforming onDisplay and showReturn functions. CSS for small screens almost setup already.

--- script.js
+++ script.js
@@ -1,79 +1,217 @@
-/////////////////////////////// creating data structures  ////////////////////////////////////////////
+//////////////////////////////////////////////////////////////// CALCULATOR ///////////////////////////////////////////////////////////////////////////////
 
-let keyInputs = []; // receives inputs from keys pressed and stores them as strings
+/* perhaps, insert a short summary to help navigate through the code */
 
-let resultStorageArray = []; // stores (final and intermediate) results of the calculations 
 
-const operators = { //object containing operators and the respective calculations they produce (as functions)
+///////////////////////////////////////////// creating data structures /////////////////////////////////////////////////
+
+let inputs = []; // receives inputs from pressed keys and stores them as strings
+
+let displayArray = []; // strings to be formatted for showing on display 
+
+let calculationStorageArray = []; 
+
+const operators = { 
   '+':  (a, b) => a + b,
   '-':  (a, b) => a - b,
   '*':  (a, b) => a * b,
   '/':  (a, b) => a / b,
   '^':  (a, b) => a ** b,
-  'log': 'customLogFunc(a, b)', // create customLogFunc, uhasuh
-  'root': "",// is there a need to build a custom root function? maybe
+  'log': 'customLogFunc(a, b)', 
+  'root': "",
 };
 
-/////////////////////////////// creating functions  ///////////////////////////////////////////////
+const utilities = { 
+  '[': '[',
+  ']': ']',
+  ',': ',',
+  'backspace': function () {
+    // this function shall only erase the last digit on the display
+    let auxArray = [];
+    auxArray = inputs[inputs.length-1].split('');
+    auxArray.pop();
+    inputs[inputs.length-1] = auxArray.join('');
 
-function onDisplay() { //function for showing keyInputs array elements on the display
+    //statements with conditional operator: '?' ----> QUESTION: is this more readable then the traditional if statement?
+    inputs[inputs.length - 1].length == 0 ? inputs.pop() : null; // if last input element has length zero, remove it
+    inputs.length > 0 ? display.textContent = inputs.join(' ') : display.textContent = 0; 
+  },
+  'display_clearing': function () {
+    inputs = []; 
+    displayArray = [];
+    calculationStorageArray = [];
+    display.textContent = 0;
+  },
+};
+
+
+
+/////////////////////////////////////// creating functions  ///////////////////////////////////////////////////
+
+function onDisplay() { //function for showing 'inputs' elements on the display
+
   /*
-    short description of the function. e. g. "this function does 'n' things and has 'm' parts"
-  */ 
-  keyInputs.push(this.textContent); // adding input to keyInputs  
+    short description of the function. e. g. "this function has 'm' parts" that do 'n' things
+      also: there is need to make if statements relative to the brackets, in order that they are kept separately as elements of the inputs
 
-  if (operators.hasOwnProperty(keyInputs[0]) && (keyInputs.length == 1)) { // that is, if the first input of keyInputs is an operator, remove it
-     keyInputs.shift(); 
-  } else if (operators.hasOwnProperty(keyInputs[keyInputs.length - 2]) && operators.hasOwnProperty(keyInputs[keyInputs.length - 1]))  {// that is, if the previously added element is an operator, you cannot add another one
-     keyInputs.pop(); 
-  } else {//revious (i-1) array element is a number){
-     console.log(keyInputs); // just for testing
-     display.textContent = keyInputs.join(' '); 
+    finally, inputs is copied to displayArray so it can be rightly formatted before displayed
+
+  */ 
+
+
+  inputs.push(this.textContent); // adds a single digit input to inputs
+
+
+
+  ////////// conditional statements
+
+  // OBSERVATION: 
+  // maybe you can unite all the operator-related rules into a single one: 
+  // if an operator is added to the array, the previous element has to be a number in order to the added operator not be removed
+
+  //// dealing with operators & utilities
+  if (operators.hasOwnProperty(inputs[0])) { // if the first input is an operator, it's removed
+    inputs.shift(); 
+  } 
+  else if ( // if the previous element is an operator and another one is added, the latter is removed
+    operators.hasOwnProperty(inputs[inputs.length - 2]) && 
+    operators.hasOwnProperty(inputs[inputs.length - 1])
+  ) { 
+    inputs.pop(); 
   }
+  else if (utilities.hasOwnProperty(inputs[inputs.length - 1])) { // if brackets are typed, they are added to inputs separately from numbers
+    display.textContent = inputs.join(' ');
+  } else if ( // so things like '55 + [ + 12 - 2 ]' cannot happen
+    operators.hasOwnProperty(inputs[inputs.length - 1]) &&
+    utilities[inputs[inputs.length - 2]] === utilities['[']
+  ) {
+    inputs.pop();
+  } else if (inputs[inputs.length - 1] === utilities[','] || inputs[inputs.length - 1] === utilities[',']
+  
+  ) {
+
+
+  }
+
+  //// dealing with numbers
+  // zeroes
+  else if ( 
+    (inputs[inputs.length - 1] === '0') && 
+    (
+      (operators.hasOwnProperty(inputs[inputs.length - 2])) || 
+      (utilities.hasOwnProperty(inputs[inputs.length - 2])) 
+    )
+  ) 
+  { // if 'zero' is inserted right after an operator or utility character, it's removed
+    inputs.pop();
+    display.textContent = inputs.join(' ');
+  } 
+  else if (
+    (inputs[inputs.length - 1] === '0') && 
+    (inputs.length == 1)
+  ) 
+  { // if zero is typed as first input, it's removed
+    inputs.pop();
+    display.textContent = '0';
+  } 
+  
+  // other numbers
+  else if (
+    !Number.isNaN(parseFloat(inputs[inputs.length - 2])) && 
+    !operators.hasOwnProperty(inputs[inputs.length - 1]) && 
+    !utilities.hasOwnProperty(inputs[inputs.length - 1])
+  ) 
+  { 
+    // when you're typing a number onto a previous number
+    inputs[inputs.length - 2] += inputs[inputs.length - 1]; // concatenate it with the newly added element 
+    inputs.pop(); // and pop out the new single digit
+    display.textContent = inputs.join(' '); 
+  }
+  else { 
+    display.textContent = inputs.join(' '); 
+  }   // left the case of typing single digits
+
+
+  /////// formatting
+
+  // const
+  numberFormatter = Intl.NumberFormat('pt-br'); // using a formatting method from built-in object 'Intl'
+  
+  for(let i = 0; i < inputs.length; i++ ) {
+    
+    if ((operators.hasOwnProperty(inputs[i])) || (utilities.hasOwnProperty(inputs[i]))) {
+      displayArray[i] = inputs[i];
+    } else {
+      displayArray[i] = numberFormatter.format(parseFloat(inputs[i]));
+    }
+  }
+
+  display.textContent = displayArray.join(' ');
+  console.log('inputs');
+  console.log(inputs);
+  console.log('displayArray');
+  console.log(displayArray);
 };
+
+
+
 
 
 function showResult() { //function for showing result calculation (when '=' is pressed)
 
-  /* short description of the function. e. g. "this function does 'n' things and has 'm' parts" */ 
+  /* short description of the function, e. g. "this function does 'n' things and has 'm' parts" */ 
 
   //This function consists of two parts:
   //    converting string representations of numbers into float type; and
   //    analyzing string representations of operators and retrieving an equivalent, but usable, symbol. (WHAT?)
-  
-  for(let i = 0; i < keyInputs.length; i++) {
-    //converting into floats only strings that represent numbers 
-        // you need to join together strings of consecutive numbers before converting them into floats 
-        // i. e. the subset array ['1', '2', '3'] needs to become ['123'] before becoming [123]
-    if (parseFloat(keyInputs[i]) != NaN) { //in other words, if keyInputs[i] is a number
+      
+  // Loop for identifying a openning bracket, inside which calculations have priority
+
+  // perhaps its better to join all of the loops below into an unique loop and to evaluate each element in inputs only once
+
+  numberFormatter = Intl.NumberFormat('pt-br'); // using a formatting method from built-in object 'Intl'
+
+  let inputsCopy = inputs;
+
+  for (let i = 0; i < inputsCopy.length; i++) {
+
+    if (!Number.isNaN(parseFloat(inputsCopy[i]))) { //in other words, if inputsCopy[i] is a number
       /* when the i-th element is a number: 
         the element i + 1 can either be a number or an operator:
       */
-    // case one: the next (i + 1) element in keyInputs is an operator: 
-      if (operators.hasOwnProperty(keyInputs[i+1])) {
-        let operand1 = parseFloat(keyInputs[i]);
-        let operand2 = parseFloat(keyInputs[i+2]);
-        resultStorageArray[i] = operators[keyInputs[i+1]](operand1, operand2);
-        display.textContent = resultStorageArray[i].toString();
-      };     
-    // case two: the next (i + 1) element in keyInputs is a number:
-      if (parseFloat(keyInputs[i+1]) != NaN) {
-        let auxArray = []; // auxiliary array
-        auxArray[i] = keyInputs[i] + keyInputs[i + 1];
-        keyInputs[i] = auxArray[i];
+      console.log((!Number.isNaN(parseFloat(inputsCopy[i]))));
 
-        //resultStorageArray.push(operators[keyInputs[i-1]](parseFloat(keyInputs[i]), parseFloat(keyInputs[i-2])));
-        //resultStorageArray[i] = operators[keyInputs[i+1]](operand1, operand2);
-        display.textContent = resultStorageArray[i].toString();
-      }; 
+      if (operators.hasOwnProperty(inputsCopy[i+1])) {
+        let operand1 = parseFloat(inputsCopy[i]);
+        let operand2 = parseFloat(inputsCopy[i+2]);
+        console.log(operators[inputsCopy[i+1]](operand1, operand2));
+        calculationStorageArray.push(operators[inputsCopy[i+1]](operand1, operand2));
+        display.textContent = numberFormatter.format(calculationStorageArray[calculationStorageArray.length-1].toString());
+      };     
+      console.log(calculationStorageArray);
     } 
-  }
+    
+    if (utilities['['] === inputsCopy[i]) {
+
+      for (let j = i + 1; j < inputsCopy.length; j++) {
+
+        if (utilities[']'] === inputsCopy[j]) {
+          calculationStorageArray.push(showResult(inputsCopy.slice(i + 1, j))); // reminder: j is not included
+          calculationStorageArray.unshift(showResult(calculationStorageArray));
+          calculationStorageArray.splice(1)
+          // calculationStorageArray = showResult(calculationStorageArray); 
+        }
+      }
+    };      
+  }    
+
+  
 };
 
     
-
-
 ///////////////////////// declaring variables relative to calculator interface ////////////////////////////////////////////
+
+/* short explanation text*/
 
 // display
 
@@ -92,7 +230,7 @@ const rootOperator = document.getElementById('root-operator');
 //utility keys
 
 const resultKey = document.getElementById('result-key');
-const displayClearing = document.getElementById('display-clearing');
+const displayClearing = document.getElementById('display-clearing-key');
 const backspaceKey = document.getElementById('backspace-key');
 const openningBracketsKey = document.getElementById('openning-brackets-key');
 const closingBracketsKey = document.getElementById('closing-brackets-key');
@@ -114,6 +252,8 @@ const signInverter = document.getElementById('sign-inverter');
 
 //////////////////////////////// creating event listeners ///////////////////////////////////////////////
 
+/* short explanation text*/
+
 /* There are two ways of making commands and inputs:
       by clicking on the virtual keys; or 
       by pressing the corresponding physical keys on the keyboard.
@@ -134,9 +274,8 @@ rootOperator.addEventListener('click', onDisplay);
 //utility keys
 
 resultKey.addEventListener('click', showResult);
-/* resultKey.addEventListener('click', );
-displayClearing.addEventListener('click', );
-backspaceKey.addEventListener('click', ); */
+displayClearing.addEventListener('click', utilities.display_clearing);
+backspaceKey.addEventListener('click', utilities['backspace']); 
 openningBracketsKey.addEventListener('click', onDisplay);
 closingBracketsKey.addEventListener('click', onDisplay);
 
